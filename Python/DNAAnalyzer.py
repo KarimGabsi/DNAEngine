07dba0a4b95f5b01d529059c9d646fcc8573ce00
King G <King-G@users.noreply.github.com>
2018-04-09 18:11:09 +0200

    Fully functional with bytes interpreter as int as input

--- Python/DNAAnalyzer.py
+++ Python/DNAAnalyzer.py
@@ -2,7 +2,17 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
+def dash():
+    dash = []
+    dash.append("[")
+    for i in range(0, 20):
+        dash.append("=")
+    dash.append("]")
+    return str(dash)
 def main():
+    
+    displayresult = []
+    displayresult.append(dash())
     
     filepath = "./DNAs"  
     import DNA_Reader as dna_reader
@@ -10,7 +20,7 @@ def main():
     #Read DNA Folder
     dna_reader.ReadAllInFolder(filepath)
     
-    # X (chromosomes)
+    # X (DNA)
     X = dna_reader.Stream.X
     # Y (results)
     Y = dna_reader.Stream.Y
@@ -19,40 +29,59 @@ def main():
     import numpy as np
     X = np.array(X)
     Y = np.array(Y)
-       
+    
     #Feed(input): number of bits (bytes*8)
     feed = dna_reader.GetFeed()
     #Output: Either Black(0) or White(1), 1 output
     output = dna_reader.GetOutputNodes()
     
-    #njobs (processes) set to 1 if on windows (slower), set to n=number of processes on linux
-    njobs = 1 #if linux ex. 4 (4 processes))
+    displayresult.append("DNA READER")
+    displayresult.append("X (DNA) Shape: {}".format(X.shape))
+    displayresult.append("Y (Result) Shape: {}".format(X.shape))
+    displayresult.append("Feed: {}".format(feed))
+    displayresult.append("Output: {}".format(output))
+    displayresult.append(dash())
     
+    displayresult.append(dash())
+    displayresult.append("BEST PARAMETER GENERATION")
+    #njobs (processes) set to 1 if on windows (slower), set to n=number of processes on linux
+    njobs = -1 #if linux ex. 4 (4 processes))
+      
     #Set parameters to test on....
-    parameters = {'batch_size': [4, 5, 10],
-                  'epochs': [10, 50],
+    parameters = {'batch_size': [10, 20],
+                  'epochs': [10, 50, 100],
                   'optimizer': ['adam', 'rmsprop'],}
+    displayresult.append("Parameters to test: {}".format(parameters))
     #Generate best parameters (WARNING: takes a while...)
-    #GenerateBestParameterSet(X, Y, feed, output, parameters, njobs)
-    #Best Parameters: {'batch_size': 4, 'dropout': 0.2, 'epochs': 50, 'layercount': 4, 'optimizer': 'rmsprop'}
-    #Best Accuracy: 0.5
+    bestparameters = GenerateBestParameterSet(X, Y, feed, output, parameters, njobs)
+    displayresult.append("Parameters Result: {}".format(bestparameters))
+    displayresult.append(dash())
+    
+    displayresult.append(dash())
+    displayresult.append("GENERATE ANN USING BEST PARAMETERS")
     
     #Use best ANN
     #Set optimizer to rmsprop
-    optimizer = 'rmsprop'
+    optimizer = 'adam'
     #Set batchsize
     batchsize = 10
     #Set epochs
     epochs = 100
     
-    classifier = CreateANN(X,Y,feed, output, optimizer, batchsize, epochs)   
+    classifier = CreateANN(X,Y,feed, output, optimizer, batchsize, epochs) 
+    displayresult.append("Classifier: {}".format(classifier.get_config()))
     
-    #EvaluateANN(X, Y, feed, output, optimizer, layercount, dropout, batchsize, epochs, njobs)
-    count = 0
-    for layer in classifier.layers: 
-        print (layer.get_weights())
-        count +=1
-    print ("Weights: {}".format(count))
+    evaluation = EvaluateANN(X, Y, feed, output, optimizer, batchsize, epochs)
+    displayresult.append("Evaluation: {}".format(evaluation))
+    displayresult.append(dash())
+    
+    displayresult.append(dash())
+    displayresult.append("PREDICTING ONE SPECIMEN")
+    #count = 0
+    #for layer in classifier.layers: 
+        #print (layer.get_weights())
+        #count +=1
+    #print ("Weights: {}".format(count))
     
     #Read one specimen and test if is white or black
     filepath = "./DNAs/Mouse10"  
@@ -67,13 +96,23 @@ def main():
     X = np.array(X)
     Y = np.array(Y)
     
+    displayresult.append("ONE SPECIMEN")
+    displayresult.append("Filepath : {}".format(filepath))
+    displayresult.append("X (DNA) Shape: {}".format(X.shape))
+    displayresult.append("Y (Result) Shape: {}".format(X.shape))
+    displayresult.append("Feed: {}".format(feed))
+    displayresult.append("Output: {}".format(output))
+    
     new_prediction = classifier.predict(X)
     outcome = (new_prediction > 0.5)
     if(outcome):
-        print("Mouse prediction is White ({})".format(new_prediction))
+        displayresult.append("Mouse prediction is White ({})".format(new_prediction))
     else:
-        print("Mouse prediction is Black ({})".format(new_prediction))
+        displayresult.append("Mouse prediction is Black ({})".format(new_prediction))
     
+    displayresult.append(dash())
+    print(np.array(displayresult))
+
     
 
 def GenerateBestParameterSet(X, Y, feed, output, parameters, njobs):
@@ -81,18 +120,17 @@ def GenerateBestParameterSet(X, Y, feed, output, parameters, njobs):
     #Set Standard Parameters
     dna_ann.SetANNParameters(feed, output ,None)
     #Generate best ANN using the parameters
-    dna_ann.GenerateBestANN(X,Y, parameters, njobs)
+    return dna_ann.GenerateBestParameterSet(X,Y, parameters, njobs)
 
 def CreateANN(X, Y, feed, output, optimizer, batchsize, epochs):  
     import DNA_ANN as dna_ann  
     dna_ann.SetANNParameters(feed, output, optimizer)
-    classifier = dna_ann.CreateANN(X, Y, batchsize, epochs)
-    return classifier
+    return dna_ann.CreateANN(X, Y, batchsize, epochs)
 
-def EvaluateANN(X,Y,feed, output, optimizer, batchsize, epochs, njobs):
+def EvaluateANN(X,Y,feed, output, optimizer, batchsize, epochs):
     import DNA_ANN as dna_ann
     dna_ann.SetANNParameters(feed, output, optimizer)
-    dna_ann.EvaluateANN(X, Y, batchsize, epochs, njobs) 
+    return dna_ann.EvaluateANN(X, Y, batchsize, epochs) 
     
 if __name__ == "__main__":
     __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
--- Python/DNA_ANN.py
+++ Python/DNA_ANN.py
@@ -15,7 +15,7 @@ def SetANNParameters(feed, output, optimizer):
     StandardParameters.Optimizer = optimizer
 
 #Libraries
-def GenerateBestANN(X,Y, parameters, njobs):  
+def GenerateBestParameterSet(X,Y, parameters, njobs):  
     
     #Splitting the dataset into Training set and test set
     from sklearn.model_selection import train_test_split
@@ -31,11 +31,15 @@ def GenerateBestANN(X,Y, parameters, njobs):
                                scoring = 'accuracy',
                                cv = 10, n_jobs=njobs)
     grid_search = grid_search.fit(X_train, Y_train)
-    #best_parameters = grid_search.best_params_
-    #best_accuracy = grid_search.best_score_
     
-    #print ("Best Parameters: {0}".format(best_parameters))
-    #print ("Best Accuracy: {0}".format(best_accuracy))
+    bestparameters = []
+    best_parameters = grid_search.best_params_
+    best_accuracy = grid_search.best_score_
+    
+    bestparameters.append("Best Parameters: {0}".format(best_parameters))
+    bestparameters.append("Best Accuracy: {0}".format(best_accuracy))
+    
+    return bestparameters
 
 def CreateANN(X, Y, batchsize, epochs):
     #Splitting the dataset into Training set and test set
@@ -59,7 +63,7 @@ def CreateANN(X, Y, batchsize, epochs):
     
     return classifier
 
-def EvaluateANN(X, Y, batchsize, epochs, njobs):
+def EvaluateANN(X, Y, batchsize, epochs):
     #Splitting the dataset into Training set and test set
     from sklearn.model_selection import train_test_split
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.5, random_state = 0)
@@ -69,13 +73,16 @@ def EvaluateANN(X, Y, batchsize, epochs, njobs):
     from sklearn.model_selection import cross_val_score
     
     classifier = KerasClassifier(build_fn = BuildStandardClassifier, batch_size = batchsize, epochs = epochs)
-    accuracies = cross_val_score(estimator = classifier, X = X_train, y = Y_train, cv = 10, n_jobs= njobs)
+    accuracies = cross_val_score(estimator = classifier, X = X_train, y = Y_train, cv = 10)
     
     mean = accuracies.mean()
     variance = accuracies.std()
     
-    print ("Mean Accuracy: {0}".format(mean))
-    print ("Variance: {0}".format(variance))
+    evaluation = []
+    evaluation.append("Mean Accuracy: {0}".format(mean))
+    evaluation.append("Variance: {0}".format(variance))
+    
+    return evaluation
         
 def BuildStandardClassifier():
     return BuildClassifier(StandardParameters.Feed, 
@@ -92,7 +99,7 @@ def BuildClassifier(feed, output, optimizer):
     classifier = Sequential() 
 
     #Adding the input layer and first hidden layer   
-    classifier.add(Dense(units = int(feed / 2), kernel_initializer = 'uniform', activation = 'relu', input_dim = feed))  
+    classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu', input_dim = feed))  
     classifier.add(Dropout(rate = 0.2)) #Disable % of nodes
     
     #Adding the output layer
