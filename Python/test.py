a19712441a279ebd0b87730fce55bb1db1918394
King G <King-G@users.noreply.github.com>
2018-04-14 12:41:23 +0200

    Delete test.py

--- Python/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-intSize = sys.getsizeof(int())
-print (intSize)
-bitstoint  = int('1111111111111111', 2)
-print (bitstoint)
-print (sys.maxsize)
-maxsizeBits = "{0:b}".format(sys.maxsize)
-print (len(maxsizeBits) +1)
