5f24ec768168104bb84f98ec461c7ae902d6b6bd
King G <King-G@users.noreply.github.com>
2018-04-14 12:42:38 +0200

    Multiple platform support with example

---
+++ Python/Example.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+def main():
+    import sys
+    maxsize = "{0:b}".format(sys.maxsize)
+    maxsize = len(maxsize) +1
+    print ("Current system: {}-bit".format(maxsize))
+    
+    #If on windows script will run very slowly, if on linux script will utilze all cpus
+    os = sys.platform
+    print(os)
+    
+    import DNA_Analyzer
+    filepath = "./DNAs"
+    #precision offset must be between 2 and 64-bit, 32 if on 32-bitsystem
+    dna_analyzer = DNA_Analyzer.DNA_Analyzer(maxsize,os)
+    dna_analyzer.Load(filepath)
+    print(dna_analyzer.displayresult)
+    
+    #Read one specimen and test if is white or black
+    specimen = "Mouse10"
+    dna_analyzer.PredictOneSpecimen(filepath, specimen)
+    print(dna_analyzer.displayresult)
+
+if __name__ == "__main__": main() 
