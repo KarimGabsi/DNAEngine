9d688df90f8d2a6096f1e09f7d66adce73170c9f
King G <King-G@users.noreply.github.com>
2018-04-15 15:48:06 +0200

    Added example precisionoffset

--- Python/Example.py
+++ Python/Example.py
@@ -12,7 +12,9 @@ def main():
     import DNA_Analyzer
     filepath = "./DNAs"
     #precision offset must be between 2 and 64-bit, 32 if on 32-bitsystem
-    dna_analyzer = DNA_Analyzer.DNA_Analyzer(maxsize,os)
+    #example using 25 bytes as dna string, lets use precisionoffset of 8
+    precisionoffset = 8    
+    dna_analyzer = DNA_Analyzer.DNA_Analyzer(precisionoffset,os)
     dna_analyzer.Load(filepath)
     print(dna_analyzer.displayresult)
     
